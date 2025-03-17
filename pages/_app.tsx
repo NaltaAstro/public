738e3969e319035199c2d2087a14e961d300c5a4
NaltaAstro <andrelusca16@gmail.com>
2025-03-17 18:07:55 -0300

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from "next/app"
+import Head from "next/head"
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Tenho Pedido</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
+
