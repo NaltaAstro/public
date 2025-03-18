28496462834b35ad064fec48b64ffc718d689e4f
NaltaAstro <andrelusca16@gmail.com>
2025-03-17 21:23:22 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,11 +4,33 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/:path*',
-        destination: 'https://controle-bc.bubbleapps.io/:path*',
+        source: "/:path*",
+        destination: "/api/:path*",
       },
-    ];
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ]
   },
 }
 
 module.exports = nextConfig
+
